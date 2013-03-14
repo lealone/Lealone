9b76423c35b307c551a24f47d86cace954e20433
Honghua Zhu <zhh200910@gmail.com>
2013-03-14 10:13:20 +0800

    重构omid项目的TSOHandler类

--- lealone-omid/src/main/java/com/codefollower/lealone/omid/tso/TSOHandler.java
+++ lealone-omid/src/main/java/com/codefollower/lealone/omid/tso/TSOHandler.java
@@ -34,7 +34,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.netty.buffer.ChannelBuffer;
-import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.ChannelHandlerContext;
@@ -69,37 +68,71 @@ public class TSOHandler extends SimpleChannelHandler {
 
     private static final Log LOG = LogFactory.getLog(TSOHandler.class);
 
+    /*
+     * Wrapper for Channel and Message
+     */
+    public static class ChannelAndMessage {
+        ChannelHandlerContext ctx;
+        TSOMessage msg;
+
+        ChannelAndMessage(ChannelHandlerContext c, TSOMessage m) {
+            ctx = c;
+            msg = m;
+        }
+    }
+
     /**
      * Bytes monitor
      */
     public static final AtomicInteger transferredBytes = new AtomicInteger();
-    //   public static int transferredBytes = 0;
+
+    /**
+     * Returns the number of transferred bytes
+     * @return the number of transferred bytes
+     */
+    public static long getTransferredBytes() {
+        return transferredBytes.longValue();
+    }
+
     public static int abortCount = 0;
-    public static int hitCount = 0;
     public static long queries = 0;
 
     /**
      * Channel Group
      */
-    private ChannelGroup channelGroup = null;
-
-    private Map<Channel, ReadingBuffer> messageBuffersMap = new HashMap<Channel, ReadingBuffer>();
-
+    private final ChannelGroup channelGroup;
     /**
      * Timestamp Oracle
      */
-    private TimestampOracle timestampOracle = null;
+    private final TimestampOracle timestampOracle;
 
     /**
      * The wrapper for the shared state of TSO
      */
-    private TSOState sharedState;
+    private final TSOState sharedState;
 
-    private FlushThread flushThread;
-    private ScheduledExecutorService scheduledExecutor;
+    private final FlushThread flushThread;
+    private final ScheduledExecutorService scheduledExecutor;
+
     private ScheduledFuture<?> flushFuture;
+    private boolean finish;
 
-    private ExecutorService executor;
+    private final Map<Channel, ReadingBuffer> messageBuffersMap = new HashMap<Channel, ReadingBuffer>();
+    private final Object sharedMsgBufLock = new Object();
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private final Object callbackLock = new Object();
+    private final AddRecordCallback noCallback = new AddRecordCallback() {
+        @Override
+        public void addRecordComplete(int rc, Object ctx) {
+        }
+    };
+
+    private final Runnable createAbortedSnaphostTask = new Runnable() {
+        @Override
+        public void run() {
+            createAbortedSnapshot();
+        }
+    };
 
     /**
      * Constructor
@@ -109,9 +142,7 @@ public class TSOHandler extends SimpleChannelHandler {
         this.channelGroup = channelGroup;
         this.timestampOracle = state.getTimestampOracle();
         this.sharedState = state;
-    }
 
-    public void start() {
         this.flushThread = new FlushThread();
         this.scheduledExecutor = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
             @Override
@@ -122,16 +153,87 @@ public class TSOHandler extends SimpleChannelHandler {
                 return t;
             }
         });
-        this.flushFuture = scheduledExecutor.schedule(flushThread, TSOState.FLUSH_TIMEOUT, TimeUnit.MILLISECONDS);
-        this.executor = Executors.newSingleThreadExecutor();
     }
 
-    /**
-     * Returns the number of transferred bytes
-     * @return the number of transferred bytes
-     */
-    public static long getTransferredBytes() {
-        return transferredBytes.longValue();
+    private void createAbortedSnapshot() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream toWAL = new DataOutputStream(baos);
+
+        long snapshot = sharedState.hashmap.getAndIncrementAbortedSnapshot();
+
+        try {
+            toWAL.writeByte(LoggerProtocol.SNAPSHOT);
+            toWAL.writeLong(snapshot);
+            for (AbortedTransaction aborted : sharedState.hashmap.halfAborted) {
+                // ignore aborted transactions from last snapshot
+                if (aborted.getSnapshot() < snapshot) {
+                    toWAL.writeByte(LoggerProtocol.ABORT);
+                    toWAL.writeLong(aborted.getStartTimestamp());
+                }
+            }
+        } catch (IOException e) {
+            // can't happen
+            throw new RuntimeException(e);
+        }
+
+        sharedState.addRecord(baos.toByteArray(), noCallback, null);
+    }
+
+    public void start() {
+        flushFuture = scheduledExecutor.schedule(flushThread, TSOState.FLUSH_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    public void stop() {
+        finish = true;
+    }
+
+    private void flush() {
+        synchronized (sharedState) {
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("Adding record, size: " + sharedState.baos.size());
+            }
+            sharedState.addRecord(sharedState.baos.toByteArray(), new AddRecordCallback() {
+                @Override
+                public void addRecordComplete(int rc, Object ctx) {
+                    if (rc != Code.OK) {
+                        LOG.warn("Write failed: " + LoggerException.getMessage(rc));
+                    } else {
+                        synchronized (callbackLock) {
+                            @SuppressWarnings("unchecked")
+                            ArrayList<ChannelAndMessage> theBatch = (ArrayList<ChannelAndMessage>) ctx;
+                            for (ChannelAndMessage cam : theBatch) {
+                                Channels.write(cam.ctx, Channels.succeededFuture(cam.ctx.getChannel()), cam.msg);
+                            }
+                        }
+                    }
+                }
+            }, sharedState.nextBatch);
+            sharedState.nextBatch = new ArrayList<ChannelAndMessage>(sharedState.nextBatch.size() + 5);
+            sharedState.baos.reset();
+            if (flushFuture.cancel(false)) {
+                flushFuture = scheduledExecutor.schedule(flushThread, TSOState.FLUSH_TIMEOUT, TimeUnit.MILLISECONDS);
+            }
+        }
+    }
+
+    private class FlushThread implements Runnable {
+        @Override
+        public void run() {
+            if (finish) {
+                return;
+            }
+            if (sharedState.nextBatch.size() > 0) {
+                synchronized (sharedState) {
+                    if (sharedState.nextBatch.size() > 0) {
+                        if (LOG.isTraceEnabled()) {
+                            LOG.trace("Flushing log batch.");
+                        }
+                        flush();
+                    }
+                }
+            }
+            flushFuture = scheduledExecutor.schedule(flushThread, TSOState.FLUSH_TIMEOUT, TimeUnit.MILLISECONDS);
+        }
     }
 
     /**
@@ -141,6 +243,7 @@ public class TSOHandler extends SimpleChannelHandler {
     public void channelInterestChanged(ChannelHandlerContext ctx, ChannelStateEvent e) {
     }
 
+    @Override
     public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
         channelGroup.add(ctx.getChannel());
     }
@@ -152,6 +255,12 @@ public class TSOHandler extends SimpleChannelHandler {
         }
     }
 
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+        LOG.warn("TSOHandler: Unexpected exception from downstream.", e.getCause());
+        Channels.close(e.getChannel());
+    }
+
     /**
      * Handle receieved messages
      */
@@ -160,40 +269,21 @@ public class TSOHandler extends SimpleChannelHandler {
         Object msg = e.getMessage();
         if (msg instanceof TimestampRequest) {
             handle((TimestampRequest) msg, ctx);
-            return;
         } else if (msg instanceof CommitRequest) {
             handle((CommitRequest) msg, ctx);
-            return;
         } else if (msg instanceof FullAbortRequest) {
             handle((FullAbortRequest) msg, ctx);
-            return;
         } else if (msg instanceof CommitQueryRequest) {
             handle((CommitQueryRequest) msg, ctx);
-            return;
-        }
-    }
-
-    public void handle(AbortRequest msg, ChannelHandlerContext ctx) {
-        synchronized (sharedState) {
-            DataOutputStream toWAL = sharedState.toWAL;
-            try {
-                toWAL.writeByte(LoggerProtocol.ABORT);
-                toWAL.writeLong(msg.startTimestamp);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            abortCount++;
-            sharedState.processAbort(msg.startTimestamp);
-            synchronized (sharedMsgBufLock) {
-                queueHalfAbort(msg.startTimestamp);
-            }
+        } else if (msg instanceof AbortRequest) {
+            handle((AbortRequest) msg, ctx);
         }
     }
 
     /**
      * Handle the TimestampRequest message
      */
-    public void handle(TimestampRequest msg, ChannelHandlerContext ctx) {
+    private void handle(TimestampRequest msg, ChannelHandlerContext ctx) {
         long timestamp;
         synchronized (sharedState) {
             try {
@@ -208,13 +298,12 @@ public class TSOHandler extends SimpleChannelHandler {
         Channel channel = ctx.getChannel();
         boolean bootstrap = false;
         synchronized (messageBuffersMap) {
-            buffer = messageBuffersMap.get(ctx.getChannel());
+            buffer = messageBuffersMap.get(channel);
             if (buffer == null) {
                 synchronized (sharedMsgBufLock) {
                     bootstrap = true;
                     buffer = sharedState.sharedMessageBuffer.getReadingBuffer(ctx);
                     messageBuffersMap.put(channel, buffer);
-                    channelGroup.add(channel);
                     LOG.warn("Channel connected: " + messageBuffersMap.size());
                 }
             }
@@ -239,58 +328,46 @@ public class TSOHandler extends SimpleChannelHandler {
         Channels.write(channel, new TimestampResponse(timestamp));
     }
 
-    ChannelBuffer cb = ChannelBuffers.buffer(10);
-
-    private boolean finish;
-
-    public static long waitTime = 0;
-    public static long commitTime = 0;
-    public static long checkTime = 0;
-
-    private Object sharedMsgBufLock = new Object();
-    private Object callbackLock = new Object();
-    private AddRecordCallback noCallback = new AddRecordCallback() {
-        @Override
-        public void addRecordComplete(int rc, Object ctx) {
-        }
-    };
-
-    private Runnable createAbortedSnaphostTask = new Runnable() {
-        @Override
-        public void run() {
-            createAbortedSnapshot();
-        }
-    };
-
-    public void createAbortedSnapshot() {
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        DataOutputStream toWAL = new DataOutputStream(baos);
-
-        long snapshot = sharedState.hashmap.getAndIncrementAbortedSnapshot();
-
-        try {
-            toWAL.writeByte(LoggerProtocol.SNAPSHOT);
-            toWAL.writeLong(snapshot);
-            for (AbortedTransaction aborted : sharedState.hashmap.halfAborted) {
-                // ignore aborted transactions from last snapshot
-                if (aborted.getSnapshot() < snapshot) {
-                    toWAL.writeByte(LoggerProtocol.ABORT);
-                    toWAL.writeLong(aborted.getStartTimestamp());
-                }
+    private void handle(AbortRequest msg, ChannelHandlerContext ctx) {
+        synchronized (sharedState) {
+            DataOutputStream toWAL = sharedState.toWAL;
+            try {
+                toWAL.writeByte(LoggerProtocol.ABORT);
+                toWAL.writeLong(msg.startTimestamp);
+            } catch (IOException e) {
+                LOG.error("failed to write abort wal", e);
             }
-        } catch (IOException e) {
-            // can't happen
-            throw new RuntimeException(e);
+            abortCount++;
+            sharedState.processAbort(msg.startTimestamp);
+            synchronized (sharedMsgBufLock) {
+                queueHalfAbort(msg.startTimestamp);
+            }
         }
+    }
 
-        sharedState.addRecord(baos.toByteArray(), noCallback, null);
+    /**
+     * Handle the FullAbortReport message
+     */
+    private void handle(FullAbortRequest msg, ChannelHandlerContext ctx) {
+        synchronized (sharedState) {
+            DataOutputStream toWAL = sharedState.toWAL;
+            try {
+                toWAL.writeByte(LoggerProtocol.FULLABORT);
+                toWAL.writeLong(msg.startTimestamp);
+            } catch (IOException e) {
+                LOG.error("failed to write fullabort wal", e);
+            }
+            sharedState.processFullAbort(msg.startTimestamp);
+        }
+        synchronized (sharedMsgBufLock) {
+            queueFullAbort(msg.startTimestamp);
+        }
     }
 
     /**
      * Handle the CommitRequest message
      */
-    public void handle(CommitRequest msg, ChannelHandlerContext ctx) {
+    private void handle(CommitRequest msg, ChannelHandlerContext ctx) {
         CommitResponse reply = new CommitResponse(msg.startTimestamp);
         ByteArrayOutputStream baos = sharedState.baos;
         DataOutputStream toWAL = sharedState.toWAL;
@@ -371,7 +448,7 @@ public class TSOHandler extends SimpleChannelHandler {
                         }
                     }
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    LOG.error("failed to handle CommitRequest", e);
                 }
             } else { //add it to the aborted list
                 abortCount++;
@@ -379,7 +456,7 @@ public class TSOHandler extends SimpleChannelHandler {
                     toWAL.writeByte(LoggerProtocol.ABORT);
                     toWAL.writeLong(msg.startTimestamp);
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    LOG.error("failed to handle abort wal", e);
                 }
                 sharedState.processAbort(msg.startTimestamp);
 
@@ -427,7 +504,7 @@ public class TSOHandler extends SimpleChannelHandler {
     /**
      * Handle the CommitQueryRequest message
      */
-    public void handle(CommitQueryRequest msg, ChannelHandlerContext ctx) {
+    private void handle(CommitQueryRequest msg, ChannelHandlerContext ctx) {
         CommitQueryResponse reply = new CommitQueryResponse(msg.startTimestamp);
         reply.queryTimestamp = msg.queryTimestamp;
         synchronized (sharedState) {
@@ -451,58 +528,6 @@ public class TSOHandler extends SimpleChannelHandler {
             ctx.getChannel().write(reply);
 
             // We send the message directly. If after a failure the state is inconsistent we'll detect it
-
-        }
-    }
-
-    public void flush() {
-        synchronized (sharedState) {
-            if (LOG.isTraceEnabled()) {
-                LOG.trace("Adding record, size: " + sharedState.baos.size());
-            }
-            sharedState.addRecord(sharedState.baos.toByteArray(), new AddRecordCallback() {
-                @Override
-                public void addRecordComplete(int rc, Object ctx) {
-                    if (rc != Code.OK) {
-                        LOG.warn("Write failed: " + LoggerException.getMessage(rc));
-
-                    } else {
-                        synchronized (callbackLock) {
-                            @SuppressWarnings("unchecked")
-                            ArrayList<ChannelAndMessage> theBatch = (ArrayList<ChannelAndMessage>) ctx;
-                            for (ChannelAndMessage cam : theBatch) {
-                                Channels.write(cam.ctx, Channels.succeededFuture(cam.ctx.getChannel()), cam.msg);
-                            }
-                        }
-
-                    }
-                }
-            }, sharedState.nextBatch);
-            sharedState.nextBatch = new ArrayList<ChannelAndMessage>(sharedState.nextBatch.size() + 5);
-            sharedState.baos.reset();
-            if (flushFuture.cancel(false)) {
-                flushFuture = scheduledExecutor.schedule(flushThread, TSOState.FLUSH_TIMEOUT, TimeUnit.MILLISECONDS);
-            }
-        }
-    }
-
-    public class FlushThread implements Runnable {
-        @Override
-        public void run() {
-            if (finish) {
-                return;
-            }
-            if (sharedState.nextBatch.size() > 0) {
-                synchronized (sharedState) {
-                    if (sharedState.nextBatch.size() > 0) {
-                        if (LOG.isTraceEnabled()) {
-                            LOG.trace("Flushing log batch.");
-                        }
-                        flush();
-                    }
-                }
-            }
-            flushFuture = scheduledExecutor.schedule(flushThread, TSOState.FLUSH_TIMEOUT, TimeUnit.MILLISECONDS);
         }
     }
 
@@ -520,48 +545,6 @@ public class TSOHandler extends SimpleChannelHandler {
 
     private void queueLargestIncrease(long largestTimestamp) {
         sharedState.sharedMessageBuffer.writeLargestIncrease(largestTimestamp);
-    }
-
-    /**
-     * Handle the FullAbortReport message
-     */
-    public void handle(FullAbortRequest msg, ChannelHandlerContext ctx) {
-        synchronized (sharedState) {
-            DataOutputStream toWAL = sharedState.toWAL;
-            try {
-                toWAL.writeByte(LoggerProtocol.FULLABORT);
-                toWAL.writeLong(msg.startTimestamp);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            sharedState.processFullAbort(msg.startTimestamp);
-        }
-        synchronized (sharedMsgBufLock) {
-            queueFullAbort(msg.startTimestamp);
-        }
-    }
-
-    /*
-     * Wrapper for Channel and Message
-     */
-    public static class ChannelAndMessage {
-        ChannelHandlerContext ctx;
-        TSOMessage msg;
-
-        ChannelAndMessage(ChannelHandlerContext c, TSOMessage m) {
-            ctx = c;
-            msg = m;
-        }
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
-        LOG.warn("TSOHandler: Unexpected exception from downstream.", e.getCause());
-        Channels.close(e.getChannel());
-    }
-
-    public void stop() {
-        finish = true;
     }
 
 }
