f3b63427512b9534789e7accc26d2273e9b171c4
Honghua Zhu <zhh200910@gmail.com>
2013-04-10 20:29:08 +0800

    omid测试包名修改

--- lealone-test/src/test/java/com/codefollower/lealone/omid/transaction/TransactionManagerTest.java
+++ lealone-test/src/test/java/com/codefollower/lealone/omid/transaction/TransactionManagerTest.java
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.codefollower.lealone.omid.client;
+package com.codefollower.lealone.omid.transaction;
 
 import java.io.IOException;
 import java.util.List;
