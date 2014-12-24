3f9aff847636f14aff2426afbfdf7ef921ecf6cc
Honghua Zhu <zhh200910@gmail.com>
2014-12-24 09:52:58 +0800

    更新Copyright

--- lealone-hbase/src/main/java/org/lealone/hbase/dbobject/index/HBaseDelegateIndex.java
+++ lealone-hbase/src/main/java/org/lealone/hbase/dbobject/index/HBaseDelegateIndex.java
@@ -1,8 +1,19 @@
 /*
- * Copyright 2004-2013 H2 Group. Multiple-Licensed under the H2 License,
- * Version 1.0, and under the Eclipse Public License, Version 1.0
- * (http://h2database.com/html/license.html).
- * Initial Developer: H2 Group
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.lealone.hbase.dbobject.index;
 
