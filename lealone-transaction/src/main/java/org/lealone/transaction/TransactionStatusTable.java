7cd013080b31e278af26e8030c1e48671990a05c
Honghua Zhu <zhh200910@gmail.com>
2014-12-29 20:13:28 +0800

    事务相关的代码放到新的lealone-transaction子工程

---
+++ lealone-transaction/src/main/java/org/lealone/transaction/TransactionStatusTable.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lealone.transaction;
+
+public class TransactionStatusTable {
+    private final static TransactionStatusTable INSTANCE = new TransactionStatusTable();
+
+    public static TransactionStatusTable getInstance() {
+        return INSTANCE;
+    }
+
+    public synchronized void addRecord(Transaction localTransaction, byte[] allLocalTransactionNames) {
+
+    }
+
+    public boolean isFullSuccessful(String hostAndPort, long tid) {
+        return true;
+    }
+}
