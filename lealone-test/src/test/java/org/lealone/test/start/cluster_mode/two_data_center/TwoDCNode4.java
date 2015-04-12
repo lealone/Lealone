139b062a9d0fa7b2fd9dd4628ce8bd7770385081
Honghua Zhu <zhh200910@gmail.com>
2015-04-12 21:41:14 +0800

    整理测试用例

---
+++ lealone-test/src/test/java/org/lealone/test/start/cluster_mode/two_data_center/TwoDCNode4.java
@@ -0,0 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lealone.test.start.cluster_mode.two_data_center;
+
+public class TwoDCNode4 extends TwoDCNodeBase {
+    public static void main(String[] args) {
+        TwoDCNodeBase.run("lealone-rackdc2.properties", TwoDCNode4.class, args);
+    }
+
+    public TwoDCNode4() {
+        this.listen_address = "127.0.0.4";
+        this.dir = "node4";
+    }
+}
