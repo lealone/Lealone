6c36b69308d1cc8d9163d12b809de24c8a1e108e
Honghua Zhu <zhh200910@gmail.com>
2015-04-25 21:36:20 +0800

    update package.bat

--- package.bat
+++ package.bat
@@ -1,3 +1,12 @@
 @echo off
 
+set ARG=%1
+if /i "%ARG%" == "clean" goto clean
+call mvn package assembly:assembly -Dmaven.test.skip=true
+goto end
+
+:clean
 call mvn clean package assembly:assembly -Dmaven.test.skip=true
+goto end
+
+:end
