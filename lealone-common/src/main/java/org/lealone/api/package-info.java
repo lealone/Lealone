e0beed7ed0e98c9e78dcea6021f58ce5eba9c538
Honghua Zhu <zhh200910@gmail.com>
2017-04-01 17:37:48 +0800

    用package-info.java替换package.html

---
+++ lealone-common/src/main/java/org/lealone/api/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2004-2014 H2 Group. Multiple-Licensed under the MPL 2.0,
+ * and the EPL 1.0 (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
+/**
+ * Contains interfaces for user-defined extensions, such as triggers and user-defined aggregate functions.
+ */
+package org.lealone.api;
