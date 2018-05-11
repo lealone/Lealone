2c9b3b16fcdd6ac5c0d3224fbea3ec59bd6772ab
Honghua Zhu <zhh200910@gmail.com>
2018-05-11 15:05:38 +0800

    删除未使用的typequery类

--- lealone-orm/src/main/java/org/lealone/orm/typequery/PDate.java
+++ lealone-orm/src/main/java/org/lealone/orm/typequery/PDate.java
@@ -1,13 +1,13 @@
 package org.lealone.orm.typequery;
 
-import java.net.URI;
+import java.sql.Date;
 
 /**
- * URI property.
+ * Java sql Date property.
  *
  * @param <R> the root query bean type
  */
-public class PUri<R> extends PBaseValueEqual<R, URI> {
+public class PDate<R> extends PBaseDate<R, Date> {
 
     /**
      * Construct with a property name and root instance.
@@ -15,15 +15,14 @@ public class PUri<R> extends PBaseValueEqual<R, URI> {
      * @param name property name
      * @param root the root query bean instance
      */
-    public PUri(String name, R root) {
+    public PDate(String name, R root) {
         super(name, root);
     }
 
     /**
      * Construct with additional path prefix.
      */
-    public PUri(String name, R root, String prefix) {
+    public PDate(String name, R root, String prefix) {
         super(name, root, prefix);
     }
-
 }
