932c7a7549b48851fb55ba252f5d340d0fd251b6
Honghua Zhu <zhh200910@gmail.com>
2018-05-19 00:07:16 +0800

    格式化代码

--- lealone-test/src/test/resources/webroot/generated/model/User.js
+++ lealone-test/src/test/resources/webroot/generated/model/User.js
@@ -1,36 +1,36 @@
 class User extends Model {
 
-	constructor(modelTable, modelType) {
-		super(modelTable == null ? new ModelTable("TEST", "PUBLIC", "USER") : modelTable,
-				(modelType == undefined || modelType == null) ? REGULAR_MODEL : modelType);
-		this.name = new PString("NAME", this);
+    constructor(modelTable, modelType) {
+        super(modelTable == null ? new ModelTable("TEST", "PUBLIC", "USER") : modelTable,
+                (modelType == undefined || modelType == null) ? REGULAR_MODEL : modelType);
+        this.name = new PString("NAME", this);
         this.notes = new PString("NOTES", this);
         this.phone = new PInteger("PHONE", this);
         this.id = new PLong("ID", this);
         
         Object.defineProperty(this, "p_name", {
-        	get: function(){
-        		return this.name.get();
-        	},
-        	set: function(newValue){
-        		this.name.set(newValue);
-        	}
+            get: function(){
+                return this.name.get();
+            },
+            set: function(newValue){
+                this.name.set(newValue);
+            }
         });
         Object.defineProperty(this, "p_notes", {
-        	get: function(){
-        		return this.notes.get();
-        	},
-        	set: function(newValue){
-        		this.notes.set(newValue);
-        	}
+            get: function(){
+                return this.notes.get();
+            },
+            set: function(newValue){
+                this.notes.set(newValue);
+            }
         });
         Object.defineProperty(this, "p_phone", {
-        	get: function(){
-        		return this.phone.get();
-        	},
-        	set: function(newValue){
-        		this.phone.set(newValue);
-        	}
+            get: function(){
+                return this.phone.get();
+            },
+            set: function(newValue){
+                this.phone.set(newValue);
+            }
         });
         super.setModelProperties([ this.name, this.notes, this.phone, this.id ]);
     }
