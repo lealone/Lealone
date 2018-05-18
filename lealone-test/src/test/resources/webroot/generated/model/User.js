f0d2ade41ed639a7c162c964554f0e199489d5e4
Honghua Zhu <zhh200910@gmail.com>
2018-05-18 23:56:30 +0800

    测试跟vue框架搭配使用

--- lealone-test/src/test/resources/webroot/generated/model/User.js
+++ lealone-test/src/test/resources/webroot/generated/model/User.js
@@ -7,6 +7,31 @@ class User extends Model {
         this.notes = new PString("NOTES", this);
         this.phone = new PInteger("PHONE", this);
         this.id = new PLong("ID", this);
+        
+        Object.defineProperty(this, "p_name", {
+        	get: function(){
+        		return this.name.get();
+        	},
+        	set: function(newValue){
+        		this.name.set(newValue);
+        	}
+        });
+        Object.defineProperty(this, "p_notes", {
+        	get: function(){
+        		return this.notes.get();
+        	},
+        	set: function(newValue){
+        		this.notes.set(newValue);
+        	}
+        });
+        Object.defineProperty(this, "p_phone", {
+        	get: function(){
+        		return this.phone.get();
+        	},
+        	set: function(newValue){
+        		this.phone.set(newValue);
+        	}
+        });
         super.setModelProperties([ this.name, this.notes, this.phone, this.id ]);
     }
 
