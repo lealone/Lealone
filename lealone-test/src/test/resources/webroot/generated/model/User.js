d31193a60d76485333de572664ffe1694ea9969d
Honghua Zhu <zhh200910@gmail.com>
2018-05-19 08:46:50 +0800

    创建一个Model代理实例避免Vue框架对不必要的字段进行拦截

--- lealone-test/src/test/resources/webroot/generated/model/User.js
+++ lealone-test/src/test/resources/webroot/generated/model/User.js
@@ -1,42 +1,54 @@
 class User extends Model {
 
     constructor(modelTable, modelType) {
-        super();
-        this.modelTable = modelTable == null ? new ModelTable("TEST", "PUBLIC", "USER") : modelTable;
-        this.modelType = (modelType == undefined || modelType == null) ? REGULAR_MODEL : modelType;
+       super();
+       this.modelTable = modelTable == null ? new ModelTable("TEST", "PUBLIC", "USER") : modelTable;
+       this.modelType = (modelType == undefined || modelType == null) ? REGULAR_MODEL : modelType;
 
-        this.name = new PString("NAME", this);
-        this.notes = new PString("NOTES", this);
-        this.phone = new PInteger("PHONE", this);
-        this.id = new PLong("ID", this);
-        
-        Object.defineProperty(this, "p_name", {
-            get: function(){
-                return this.name.get();
-            },
-            set: function(newValue){
-                this.name.set(newValue);
-            }
-        });
-        Object.defineProperty(this, "p_notes", {
-            get: function(){
-                return this.notes.get();
-            },
-            set: function(newValue){
-                this.notes.set(newValue);
-            }
-        });
-        Object.defineProperty(this, "p_phone", {
-            get: function(){
-                return this.phone.get();
-            },
-            set: function(newValue){
-                this.phone.set(newValue);
-            }
-        });
-        super.setModelProperties([ this.name, this.notes, this.phone, this.id ]);
+       this.name = new PString("NAME", this);
+       this.notes = new PString("NOTES", this);
+       this.phone = new PInteger("PHONE", this);
+       this.id = new PLong("ID", this);
+       super.setModelProperties([ this.name, this.notes, this.phone, this.id ]);
     }
 
 }
 
 User.dao = new User(null, ROOT_DAO);
+
+User.getFormInstance = function(){
+    var user = new User();
+    var userProxy = {
+    }
+    userProxy.insert = function(cb) { user.insert(cb) }
+    userProxy.delete = function(cb) { user.delete(cb) }
+    userProxy.update = function(cb) { user.update(cb) }
+    userProxy.findOne = function(cb) { user.findOne(cb) }
+    
+    Object.defineProperty(userProxy, "name", {
+        get: function(){
+            return user.name.get();
+        },
+        set: function(newValue){
+            user.name.set(newValue);
+        }
+    });
+    Object.defineProperty(userProxy, "notes", {
+        get: function(){
+            return user.notes.get();
+        },
+        set: function(newValue){
+            user.notes.set(newValue);
+        }
+    });
+    Object.defineProperty(userProxy, "phone", {
+        get: function(){
+            return user.phone.get();
+        },
+        set: function(newValue){
+            user.phone.set(newValue);
+        }
+    });
+    
+    return userProxy;
+}
