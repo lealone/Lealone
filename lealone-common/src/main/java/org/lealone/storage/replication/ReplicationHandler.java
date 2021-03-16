26edbf445ba382cc50c9c408fff856de8be35fae
Honghua Zhu <zhh200910@gmail.com>
2021-03-16 21:58:51 +0800

    删除ReplicationHandler已经废弃的同步等待代码

--- lealone-common/src/main/java/org/lealone/storage/replication/ReplicationHandler.java
+++ lealone-common/src/main/java/org/lealone/storage/replication/ReplicationHandler.java
@@ -18,20 +18,12 @@
 package org.lealone.storage.replication;
 
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
-import org.lealone.common.concurrent.SimpleCondition;
 import org.lealone.db.async.AsyncHandler;
 import org.lealone.db.async.AsyncResult;
-import org.lealone.storage.replication.exceptions.ReadFailureException;
-import org.lealone.storage.replication.exceptions.ReadTimeoutException;
-import org.lealone.storage.replication.exceptions.WriteFailureException;
-import org.lealone.storage.replication.exceptions.WriteTimeoutException;
 
 abstract class ReplicationHandler<T> implements AsyncHandler<AsyncResult<T>> {
 
-    private final long start;
     private final int totalNodes;
     private final int totalBlockFor;
 
@@ -39,16 +31,10 @@ abstract class ReplicationHandler<T> implements AsyncHandler<AsyncResult<T>> {
     protected final CopyOnWriteArrayList<AsyncResult<T>> results = new CopyOnWriteArrayList<>();
 
     private final CopyOnWriteArrayList<Throwable> exceptions = new CopyOnWriteArrayList<>();
-    private final SimpleCondition condition = new SimpleCondition();
 
-    @SuppressWarnings("rawtypes")
-    private final AtomicIntegerFieldUpdater<ReplicationHandler> failuresUpdater = AtomicIntegerFieldUpdater
-            .newUpdater(ReplicationHandler.class, "failures");
-    private volatile int failures;
     private volatile boolean successful;
 
     public ReplicationHandler(int totalNodes, int totalBlockFor, AsyncHandler<AsyncResult<T>> finalResultHandler) {
-        start = System.nanoTime();
         this.totalNodes = totalNodes;
         this.totalBlockFor = totalBlockFor;
         this.finalResultHandler = finalResultHandler;
@@ -71,88 +57,19 @@ abstract class ReplicationHandler<T> implements AsyncHandler<AsyncResult<T>> {
         results.add(result);
         if (!successful && results.size() >= totalBlockFor) {
             successful = true;
-            try {
-                onSuccess();
-            } finally {
-                signal();
-            }
+            onSuccess();
         }
     }
 
     private synchronized void handleException(Throwable t) {
         exceptions.add(t);
-        int f = failuresUpdater.incrementAndGet(this);
-        if (totalBlockFor + f >= totalNodes) {
-            try {
-                if (finalResultHandler != null) {
-                    AsyncResult<T> ar = new AsyncResult<>();
-                    ar.setCause(exceptions.get(0));
-                    finalResultHandler.handle(ar);
-                }
-            } finally {
-                signal();
+        if (totalBlockFor + exceptions.size() >= totalNodes) {
+            if (finalResultHandler != null) {
+                AsyncResult<T> ar = new AsyncResult<>();
+                ar.setCause(exceptions.get(0));
+                finalResultHandler.handle(ar);
             }
         }
     }
 
-    private void signal() {
-        condition.signalAll();
-    }
-
-    @Deprecated
-    T getResult(long rpcTimeoutMillis) {
-        await(rpcTimeoutMillis);
-        return results.get(0).getResult();
-    }
-
-    @Deprecated
-    void initCause(Throwable e) {
-        if (!exceptions.isEmpty())
-            e.initCause(exceptions.get(0));
-    }
-
-    private int ackCount() {
-        return results.size();
-    }
-
-    private void await(long rpcTimeoutMillis) {
-        // 超时时间把调用构造函数开始直到调用get前的这段时间也算在内
-        long timeout = rpcTimeoutMillis > 0
-                ? TimeUnit.MILLISECONDS.toNanos(rpcTimeoutMillis) - (System.nanoTime() - start)
-                : rpcTimeoutMillis;
-
-        boolean success;
-        try {
-            if (timeout > 0) {
-                success = condition.await(timeout, TimeUnit.NANOSECONDS);
-            } else {
-                condition.await();
-                success = true;
-            }
-        } catch (InterruptedException ex) {
-            throw new AssertionError(ex);
-        }
-        if (!successful) {
-            if (!success) {
-                int blockedFor = totalBlockFor;
-                int acks = ackCount();
-                // It's pretty unlikely, but we can race between exiting await above and here, so
-                // that we could now have enough acks. In that case, we "lie" on the acks count to
-                // avoid sending confusing info to the user (see CASSANDRA-6491).
-                if (acks >= blockedFor)
-                    acks = blockedFor - 1;
-                if (isRead())
-                    throw new ReadTimeoutException(ConsistencyLevel.QUORUM, acks, blockedFor, false);
-                else
-                    throw new WriteTimeoutException(ConsistencyLevel.QUORUM, acks, blockedFor);
-            }
-
-            if (totalBlockFor + failures >= totalNodes) {
-                if (isRead())
-                    throw new ReadFailureException(ConsistencyLevel.QUORUM, ackCount(), failures, totalBlockFor, false);
-                else
-                    throw new WriteFailureException(ConsistencyLevel.QUORUM, ackCount(), failures, totalBlockFor);
-            }
-        }
-    }
 }
