e2d1f4b007ef4777eed96105506c6b60b649b517
Honghua Zhu <zhh200910@gmail.com>
2021-05-29 14:17:50 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -25,6 +25,7 @@ if /i "%ARG%" == "-ec" goto ec
 if /i "%ARG%" == "-es" goto es
 if /i "%ARG%" == "-p" goto p
 if /i "%ARG%" == "-pc" goto pc
+if /i "%ARG%" == "-pd" goto pd
 if /i "%ARG%" == "-i" goto i
 if /i "%ARG%" == "-c" goto c
 if /i "%ARG%" == "-dt" goto dt
@@ -40,6 +41,7 @@ echo    -ec           mvn eclipse:clean
 echo    -es           mvn eclipse:eclipse -DdownloadSources=true
 echo    -p            mvn package assembly:assembly -Dmaven.test.skip=true
 echo    -pc           mvn clean package assembly:assembly -Dmaven.test.skip=true
+echo    -pd           mvn package assembly:assembly -Dmaven.test.skip=true -P database
 echo    -i            mvn install -Dmaven.test.skip=true
 echo    -c            mvn clean
 echo    -dt           mvn dependency:tree
@@ -63,8 +65,12 @@ call mvn package assembly:assembly -Dmaven.test.skip=true
 goto end
 
 :pc
-mvn clean package assembly:assembly -Dmaven.test.skip=true
-goto p
+call mvn clean package assembly:assembly -Dmaven.test.skip=true
+goto end
+
+:pd
+call mvn package assembly:assembly -Dmaven.test.skip=true -P database
+goto end
 
 :c
 call mvn clean
