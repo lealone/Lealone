1766c5e26a78da6fcff6eadc9bcb42ee39655949
Honghua Zhu <zhh200910@gmail.com>
2022-04-22 09:44:04 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -15,6 +15,7 @@ if /i "%ARG%" == "-es" goto es
 if /i "%ARG%" == "-p" goto p
 if /i "%ARG%" == "-pc" goto pc
 if /i "%ARG%" == "-pd" goto pd
+if /i "%ARG%" == "-pad" goto pad
 if /i "%ARG%" == "-i" goto i
 if /i "%ARG%" == "-c" goto c
 if /i "%ARG%" == "-dt" goto dt
@@ -30,7 +31,8 @@ echo    -ec           mvn eclipse:clean
 echo    -es           mvn eclipse:eclipse -DdownloadSources=true
 echo    -p            mvn package assembly:assembly -Dmaven.test.skip=true
 echo    -pc           mvn clean package assembly:assembly -Dmaven.test.skip=true
-echo    -pd           mvn package assembly:assembly -Dmaven.test.skip=true -P database
+echo    -pd           mvn package -Dmaven.test.skip=true -P database
+echo    -pad          mvn package assembly:assembly -Dmaven.test.skip=true -P database
 echo    -i            mvn install -Dmaven.test.skip=true
 echo    -c            mvn clean
 echo    -dt           mvn dependency:tree
@@ -58,6 +60,10 @@ call mvn clean package assembly:assembly -Dmaven.test.skip=true
 goto end
 
 :pd
+call mvn package -Dmaven.test.skip=true -P database
+goto end
+
+:pad
 call mvn package assembly:assembly -Dmaven.test.skip=true -P database
 goto end
 
