a1cabf95c0d9316329ce0d44a19dc5fca9d97ac1
Honghua Zhu <zhh200910@gmail.com>
2023-01-11 16:14:31 +0800

    执行 DDL 语句遇到对象锁时重复设置 Session 状态为 WAITING 有可能导致当前事务会被挂起

--- lealone-sql/src/main/java/org/lealone/sql/executor/YieldableLocalUpdate.java
+++ lealone-sql/src/main/java/org/lealone/sql/executor/YieldableLocalUpdate.java
@@ -23,10 +23,10 @@ public class YieldableLocalUpdate extends YieldableUpdateBase {
         int updateCount = statement.update();
         setResult(updateCount);
 
-        // 返回的值为负数时，表示当前语句无法正常执行，需要等待其他事务释放锁
-        if (updateCount < 0) {
-            session.setStatus(SessionStatus.WAITING);
-        } else {
+        // 返回的值为负数时，表示当前语句无法正常执行，需要等待其他事务释放锁。
+        // 当 updateCount<0 时不能再设置为 WAITING 状态，
+        // 一方面已经设置过了，另一方面如果此时其他事务释放锁了再设置会导致当前语句在后续无法执行。
+        if (updateCount >= 0) {
             session.setStatus(SessionStatus.STATEMENT_COMPLETED);
         }
     }
