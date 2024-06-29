c863f4497d808cb3bc3151b58b404f37b4b95c48
Honghua Zhu <zhh200910@gmail.com>
2024-06-29 10:21:15 +0800

    bug fix：通过命令行的方式用嵌入模式运行数据库时无法显示 sql> 提示符

--- lealone-main/src/main/java/com/lealone/main/Lealone.java
+++ lealone-main/src/main/java/com/lealone/main/Lealone.java
@@ -179,9 +179,11 @@ public class Lealone {
             });
             scheduler.start();
             scheduler.wakeUp(); // 及时唤醒，否则会影响启动速度
-            // 在主线程中运行，避免出现DestroyJavaVM线程
-            Thread.currentThread().setName("FsyncService");
-            TransactionEngine.getDefaultTransactionEngine().getFsyncService().run();
+            if (!embedded) {
+                // 在主线程中运行，避免出现DestroyJavaVM线程
+                Thread.currentThread().setName("FsyncService");
+                TransactionEngine.getDefaultTransactionEngine().getFsyncService().run();
+            }
         } catch (Exception e) {
             logger.error("Fatal error: unable to start lealone. See log for stacktrace.", e);
             System.exit(1);
