50efbdee53308c2e0915f26bab20efa7a54b5022
Honghua Zhu <zhh200910@gmail.com>
2026-03-30 00:03:50 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -14,8 +14,7 @@ if /i "%ARG%" == "-ec" goto ec
 if /i "%ARG%" == "-es" goto es
 if /i "%ARG%" == "-p" goto p
 if /i "%ARG%" == "-pc" goto pc
-if /i "%ARG%" == "-pd" goto pd
-if /i "%ARG%" == "-pad" goto pad
+if /i "%ARG%" == "-pai" goto pai
 if /i "%ARG%" == "-i" goto i
 if /i "%ARG%" == "-c" goto c
 if /i "%ARG%" == "-dt" goto dt
@@ -31,8 +30,7 @@ echo    -ec           mvn eclipse:clean
 echo    -es           mvn eclipse:eclipse -DdownloadSources=true
 echo    -p            mvn package assembly:assembly -Dmaven.test.skip=true
 echo    -pc           mvn clean package assembly:assembly -Dmaven.test.skip=true
-echo    -pd           mvn package -Dmaven.test.skip=true -P database
-echo    -pad          mvn package assembly:assembly -Dmaven.test.skip=true -P database
+echo    -pai          mvn package -Dmaven.test.skip=true -P ai
 echo    -i            mvn install -DskipTests
 echo    -c            mvn clean
 echo    -dt           mvn dependency:tree
@@ -59,12 +57,8 @@ goto end
 call mvn clean package assembly:assembly -Dmaven.test.skip=true
 goto end
 
-:pd
-call mvn package -Dmaven.test.skip=true -P database
-goto end
-
-:pad
-call mvn package assembly:assembly -Dmaven.test.skip=true -P database
+:pai
+call mvn package -Dmaven.test.skip=true -P ai
 goto end
 
 :c
